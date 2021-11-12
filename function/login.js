e6c9e395a41aff1c27064b1e7f24d68ffebab74a
magrogovich <mboufijline@gmail.com>
2021-11-11 16:11:41 -0800

    update

--- function/login.js
+++ function/login.js
@@ -6,17 +6,70 @@ document.getElementById('button').addEventListener('click',function(){
     day = document.getElementsByClassName('dd')[0].value
     month = document.getElementsByClassName('mm')[0].value
     year = document.getElementsByClassName('year')[0].value
-
+    verif = true
     test = true
     if ((userName.length == 0 )||( lastName.length == 0) || (email.length == 0) || (confEmail.length == 0) || (day.length == 0) || (month.length == 0) || (year.length == 0)){
         alert('fill all the coordinates')
         test = false
+        verif = false
     }
     test2 = true
     if (test == true){
         for (i = 0;i<userName.length ;i++){
-            if ((i.toUpperCase()))
+            if ((userName[i].toUpperCase()< 'A' || userName[i].toUpperCase()> 'Z')){
+                test2 = false
+                verif = false
+                break
+            }
         }
     }
+
+    if(test2 == false){
+        alert("user name can contains letter")
+    }
+    test3 = true
+    if (test == true){
+        for (i = 0;i<lastName.length ;i++){
+            if ((lastName[i].toUpperCase()< 'A' || lastName[i].toUpperCase()> 'Z')){
+                test3 = false
+                verif = false
+                break
+            }
+        }
+    }
+
+    if(test3 == false){
+        alert("user lastname can contains letter")
+    }
+
+    test4 = true
+    if (test == true){
+        for (i = 0;i<email.length ;i++){
+            if ((email[i].toUpperCase()< 'A' || email[i].toUpperCase()> 'Z') && (email[i]< '0' || email[i]> '9') && (email[i] != '@') && (email[i] != '.')){
+                test4 = false
+                verif = false
+                break
+            }
+        }
+    }
+
+    if(test4 == false){
+        alert("email is uncorrect")
+    }
+
+    test5 = true
+    if (test == true){
+        if (confEmail != email){
+            test5 = false
+            verif = false
+        }
+    }
+
+    if(test5 == false){
+        alert("email do not match")
+    }
+
+    
+
 })
 
